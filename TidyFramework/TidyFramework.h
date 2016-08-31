7e43fff4a417bc72091114696024cd6772d36b92
AthanasiusOfAlex <Louis Melahn>
2016-08-31 13:24:23 +0200

    Initial Commit

---
+++ TidyFramework/TidyFramework.h
@@ -0,0 +1,19 @@
+//
+//  TidyFramework.h
+//  TidyFramework
+//
+//  Created by Louis Melahn on 8/31/16.
+//  Copyright © 2016 Louis Melahn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TidyFramework.
+FOUNDATION_EXPORT double TidyFrameworkVersionNumber;
+
+//! Project version string for TidyFramework.
+FOUNDATION_EXPORT const unsigned char TidyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TidyFramework/PublicHeader.h>
+
+
